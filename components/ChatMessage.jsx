5e16dd8f96590ba0aadf9d269247fdd45ad07e13
Mohamed Ahmed Ghazali <MGhazali@salaambank.co.ug>
2026-06-01 13:03:19 +0300

    Use guard or AND operator (&&) to insert an if statement directly inside JSX

--- components/ChatMessage.jsx
+++ components/ChatMessage.jsx
@@ -1,18 +1,20 @@
 
 export default function ChatMessage({ message, sender }) {
 
-  if (sender === 'robot') {
-    return (
-       <div>
-         <img className="w-10 h-10 inline ml-2" src={`${sender}.png`} alt={sender === 'user' ? 'User' : 'Chatbot'} />
-        {message}
-         </div>
-    );
-  }
   return (
        <div>
+        {sender === 'robot'  && 
+        (<img className="w-10 h-10 inline ml-2" 
+          src={`${sender}.png`}  
+          alt={sender === 'user' ? 'User' : 'Chatbot'} />)
+}
         {message}
-        <img className="w-10 h-10 inline ml-2" src={`${sender}.png`} alt={sender === 'user' ? 'User' : 'Chatbot'} />
+
+        {sender === 'user' && 
+        (<img className="w-10 h-10 inline ml-2" 
+          src={`${sender}.png`} 
+          alt={sender === 'user' ? 'User' : 'Chatbot'} />)
+}
          </div>
   )
 }
