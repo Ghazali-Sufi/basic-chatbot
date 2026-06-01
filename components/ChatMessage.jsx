cde71084fde8f7d129ef229d6dd72e1c273d9f54
Mohamed Ahmed Ghazali <MGhazali@salaambank.co.ug>
2026-06-01 09:56:37 +0300

    Classified by sender

--- components/ChatMessage.jsx
+++ components/ChatMessage.jsx
@@ -1,5 +1,14 @@
 
 export default function ChatMessage({ message, sender }) {
+
+  if (sender === 'robot') {
+    return (
+       <div>
+         <img className="w-10 h-10 inline ml-2" src={`${sender}.png`} alt={sender === 'user' ? 'User' : 'Chatbot'} />
+        {message}
+         </div>
+    );
+  }
   return (
        <div>
         {message}
