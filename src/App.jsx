4244597fc035ecfbd42b0d37a8da048a07af31a8
Mohamed Ahmed Ghazali <MGhazali@salaambank.co.ug>
2026-06-04 12:06:34 +0300

    Use array to save a list of chat messages

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,53 @@ import ChatInput from '../components/ChatInput.jsx'
 import ChatMessage from '../components/ChatMessage.jsx'
 
 export default function App() {
+  const chatMessages = [
+    {
+      message: 'hello chatbot',
+      sender: 'user',
+      id: 'id1'
+        
+    },
+     {
+      message: 'Hello! How can I help you?',
+      sender: 'robot',
+      id: 'id2' 
+    },
+    {
+      message: 'Can you get me todays date?',
+      sender: 'user',
+      id: 'id3'
+    },
+    {
+      message: 'Today is June 01" sender',
+      sender: 'robot',
+      id: 'id4'
+    },
+    {
+      message: 'how about flip a coin',
+      sender: 'user',
+      id: 'id5'
+    },
+     {
+      message: 'Sure! You got tails',
+      sender: 'robot',
+      id: 'id6' 
+    },
+    
+  ]
   
   return (
     <>
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-12 px-4">
       <ChatInput />
-      <ChatMessage message="hello chatbot" sender="user" />
-      <ChatMessage message="Hello! How can I help you?" sender="robot"/>
-      <ChatMessage message="Can you get me today's date?" sender="user"/>
-      <ChatMessage message="Today is June 01" sender="robot"/>
-      <ChatMessage message="how about flip a coin" sender="user"/>
-      <ChatMessage message="Sure! You got tails" sender="robot"/>
+      {chatMessages.map((chatMessage) => {
+      return (
+        <ChatMessage 
+        message={chatMessage.message} 
+        sender={chatMessage.sender}
+        key={chatMessage.id}/>
+      )
+    })}
     </div>
     </>
   )
